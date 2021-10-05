15db3ed7908a4163c5fb0c6b8260b713ceedcc3a
mahsakooshesh <75886845+mahsakooshesh@users.noreply.github.com>
2021-10-05 22:09:06 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
 const form = document.querySelector("form");
 let search = "";
 let color = "";
-let numberOfPage =0;
+let numberOfPage = 0;
 
 const api_key = "23520112-b013bf2d41dfff6b7c3007bcf";
 let pageNumber = 1;
@@ -11,7 +11,7 @@ let counter = 0;
 let letfOverImage = 0;
 let imageAmount = 0;
 let arrayImages = [];
-let itemImage = {srcIm: "", tagIm : "", userIm: "" };
+let itemImage = { srcIm: "", tagIm: "", userIm: "" };
 let amountImageInLastPage = 0;
 
 //variables for new pictures elements
@@ -25,7 +25,7 @@ let buttonBack = document.getElementById("back");
 let container = document.getElementById("result");
 let buttons = document.getElementById("buttons");
 
-function CreatePicturesElements (){
+function CreatePicturesElements(i) {
 
     contain = document.createElement("section");
     contain.id = "contain";
@@ -34,40 +34,37 @@ function CreatePicturesElements (){
     tag = document.createElement('figcaption');
     tag.id = "tags";
     user = document.createElement('figcaption');
-    user.id = "users";                   
-    
-}
+    user.id = "users";
 
-function AppendChildFunction() {
+    tag.textContent = arrayImages[i].tagIm;
+    user.textContent = arrayImages[i].userIm;
+    img.src = arrayImages[i].srcIm;
+
     container.appendChild(contain);
     contain.appendChild(img);
     contain.appendChild(tag);
     contain.appendChild(user);
+
+    counter++;
 }
 
-async function SearchImage(urlParameter){
+async function SearchImage(urlParameter) {
     const response = await fetch(urlParameter);
     const json = await response.json();
-    numberOfPage = Math.ceil(json.totalHits/10);
+    numberOfPage = Math.ceil(json.totalHits / 10);
     json.hits.forEach(image => {
 
         let srcImage = image.previewURL;
         let tagImage = image.tags;
         let userImage = image.user;
-        itemImage = {srcIm: srcImage, tagIm : tagImage, userIm: userImage };
+        itemImage = { srcIm: srcImage, tagIm: tagImage, userIm: userImage };
         arrayImages.push(itemImage);
-        });
-    
+    });
+
     imageAmount = arrayImages.length;
-    for (let i=0;i<imageAmount;i++)
-        {                      
-          CreatePicturesElements();
-          tag.textContent = arrayImages[i].tagIm;
-          user.textContent = arrayImages[i].userIm;
-          img.src = arrayImages[i].srcIm;
-          AppendChildFunction();          
-          counter+=1;
-        }
+    for (let i = 0; i < imageAmount; i++) {
+        CreatePicturesElements(i);
+    }
 }
 
 form.onsubmit = event => {
@@ -81,7 +78,7 @@ form.onsubmit = event => {
     letfOverImage = 0;
     imageAmount = 0;
     amountImageInLastPage = 0;
-    container.textContent="";
+    container.textContent = "";
     arrayImages = [];
     buttonBack.disabled = true;
     buttonBack.disabled = true;
@@ -93,13 +90,13 @@ form.onsubmit = event => {
         q: search,
         colors: color,
         per_page: 10,
-        page : pageNumber
-        
-    })
-    url = form.action +'?'+ params.toString();
+        page: pageNumber
 
-    if(search != ""){
-        SearchImage(url); 
+    })
+    url = form.action + '?' + params.toString();
+
+    if (search != "") {
+        SearchImage(url);
         buttonBack.style.display = "";
         buttonNext.disabled = false;
         buttonNext.style.display = "";
@@ -108,26 +105,26 @@ form.onsubmit = event => {
         alert("search box cannot be empty");
         buttonBack.style.display = "none";
         buttonNext.style.display = "none";
-    }   
+    }
 }
 
 buttonNext.onclick = event => {
     event.preventDefault();
-    container.textContent= "";
+    container.textContent = "";
     arrayImages = [];
 
-    pageNumber+=1;
+    pageNumber += 1;
     const params = new URLSearchParams({
         key: api_key,
         q: search,
         colors: color,
         per_page: 10,
-        page : pageNumber
-        
+        page: pageNumber
+
     })
-    url = form.action +'?'+ params.toString();
+    url = form.action + '?' + params.toString();
     SearchImage(url);
-    if (pageNumber === numberOfPage){
+    if (pageNumber === numberOfPage) {
 
         buttonBack.disabled = false;
         buttonBack.style.display = "";
@@ -146,21 +143,21 @@ buttonNext.onclick = event => {
 }
 buttonBack.onclick = event => {
     event.preventDefault();
-    container.textContent= "";
+    container.textContent = "";
     arrayImages = [];
 
-    pageNumber-=1;
+    pageNumber -= 1;
     const params = new URLSearchParams({
         key: api_key,
         q: search,
         colors: color,
         per_page: 10,
-        page : pageNumber
-        
+        page: pageNumber
+
     })
-    url = form.action +'?'+ params.toString();
+    url = form.action + '?' + params.toString();
     SearchImage(url);
-    if (pageNumber === 1){
+    if (pageNumber === 1) {
 
         buttonBack.disabled = true;
         buttonBack.style.display = "";
@@ -169,11 +166,11 @@ buttonBack.onclick = event => {
 
     }
     else {
-    buttonBack.disabled = false;
-    buttonBack.style.display = "";
-    buttonNext.disabled = false;
-    buttonNext.style.display = "";
+        buttonBack.disabled = false;
+        buttonBack.style.display = "";
+        buttonNext.disabled = false;
+        buttonNext.style.display = "";
     }
-    
+
 
 }
